c1f55d3730aedb3e390e64fe10dd31b9cbf6358e
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-04-20 17:30:25 -0400

    Add pdf

---
+++ src/libraries/pdf/main.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+const qt6 = @import("libqt6zig");
+const qapplication = qt6.qapplication;
+const qpdfdocument = qt6.qpdfdocument;
+const qpdfview = qt6.qpdfview;
+const qpdfview_enums = qt6.qpdfview_enums;
+
+pub fn main() void {
+    const argc = std.os.argv.len;
+    const argv = std.os.argv.ptr;
+    _ = qapplication.New(argc, argv);
+
+    const document = qpdfdocument.New();
+    defer qpdfdocument.QDelete(document);
+    _ = qpdfdocument.Load(document, "assets/example.pdf");
+
+    const pdfview = qpdfview.New2();
+    defer qpdfview.DeleteLater(pdfview);
+
+    qpdfview.SetWindowTitle(pdfview, "Qt 6 PDF Example");
+    qpdfview.SetMinimumSize2(pdfview, 650, 600);
+    qpdfview.SetPageMode(pdfview, qpdfview_enums.PageMode.MultiPage);
+    qpdfview.SetZoomMode(pdfview, qpdfview_enums.ZoomMode.FitInView);
+    qpdfview.SetDocument(pdfview, document);
+
+    qpdfview.Show(pdfview);
+
+    _ = qapplication.Exec();
+}
