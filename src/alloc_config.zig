3092e63308f9d8ab492f9d08a3be781b2c059edd
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-08-13 19:55:17 -0400

    alloc_config.zig: Move custom allocator config to individual file

---
+++ src/alloc_config.zig
@@ -0,0 +1,20 @@
+const builtin = @import("builtin");
+const std = @import("std");
+
+pub fn getAllocatorConfig() std.heap.DebugAllocatorConfig {
+    if (builtin.mode == .Debug) {
+        return std.heap.DebugAllocatorConfig{
+            .safety = true,
+            .never_unmap = true,
+            .retain_metadata = true,
+            .verbose_log = false,
+        };
+    } else {
+        return std.heap.DebugAllocatorConfig{
+            .safety = false,
+            .never_unmap = false,
+            .retain_metadata = false,
+            .verbose_log = false,
+        };
+    }
+}
