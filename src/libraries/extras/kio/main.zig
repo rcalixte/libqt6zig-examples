91c8d72c1765ef7ee28e9321bd47ae5f422a9a3c
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-09-14 18:43:43 -0400

    Add kio

---
+++ src/libraries/extras/kio/main.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const qt6 = @import("libqt6zig");
+const qapplication = qt6.qapplication;
+const kfilecustomdialog = qt6.kfilecustomdialog;
+const qlabel = qt6.qlabel;
+
+pub fn main() void {
+    const argc = std.os.argv.len;
+    const argv = std.os.argv.ptr;
+    const qapp = qapplication.New(argc, argv);
+    defer qapplication.QDelete(qapp);
+
+    const dialog = kfilecustomdialog.New2();
+    defer kfilecustomdialog.QDelete(dialog);
+
+    kfilecustomdialog.SetWindowTitle(dialog, "Qt 6 KIO Example");
+
+    const label = qlabel.New3("Select a file or directory");
+
+    kfilecustomdialog.SetCustomWidget(dialog, label);
+
+    _ = kfilecustomdialog.Exec(dialog);
+}
