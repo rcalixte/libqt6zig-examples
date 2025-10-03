0f631d862391eae1e359ad7af7dfe079b5f46c73
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-10-03 16:49:02 -0400

    Add ktexteditor

---
+++ src/libraries/extras/ktexteditor/main.zig
@@ -0,0 +1,60 @@
+const std = @import("std");
+const qt6 = @import("libqt6zig");
+const C = qt6.C;
+const qapplication = qt6.qapplication;
+const qmainwindow = qt6.qmainwindow;
+const ktexteditor__editor = qt6.ktexteditor__editor;
+const qdir = qt6.qdir;
+const qurl = qt6.qurl;
+const ktexteditor__document = qt6.ktexteditor__document;
+const qfont = qt6.qfont;
+const qtoolbar = qt6.qtoolbar;
+
+const getAllocatorConfig = @import("alloc_config").getAllocatorConfig;
+const config = getAllocatorConfig();
+var gda: std.heap.DebugAllocator(config) = .init;
+const allocator = gda.allocator();
+
+var editor: C.KTextEditor__Editor = undefined;
+
+pub fn main() !void {
+    const argc = std.os.argv.len;
+    const argv = std.os.argv.ptr;
+    const qapp = qapplication.New(argc, argv);
+    defer qapplication.QDelete(qapp);
+
+    const window = qmainwindow.New2();
+    defer qmainwindow.QDelete(window);
+
+    qmainwindow.SetWindowTitle(window, "Qt 6 KTextEditor Example");
+    qmainwindow.SetMinimumSize2(window, 1300, 1150);
+
+    editor = ktexteditor__editor.Instance();
+    const doc = ktexteditor__editor.CreateDocument(editor, window);
+
+    const dir = qdir.CurrentPath(allocator);
+    defer allocator.free(dir);
+
+    const file = try std.mem.concat(allocator, u8, &.{ "file://", dir, "/src/libraries/extras/ktexteditor/main.zig" });
+    const url = qurl.New3(file);
+    defer qurl.QDelete(url);
+
+    _ = ktexteditor__document.OpenUrl(doc, url);
+    ktexteditor__document.SetModifiedOnDiskWarning(doc, true);
+
+    const view = ktexteditor__document.CreateView(doc, window, window);
+
+    const toolbar = qtoolbar.New3();
+    _ = qtoolbar.AddAction2(toolbar, "Configure");
+    qtoolbar.OnActionTriggered(toolbar, toolbarTriggered);
+
+    qmainwindow.AddToolBar2(window, toolbar);
+    qmainwindow.SetCentralWidget(window, view);
+    qmainwindow.Show(window);
+
+    _ = qapplication.Exec();
+}
+
+fn toolbarTriggered(self: ?*anyopaque, _: ?*anyopaque) callconv(.c) void {
+    ktexteditor__editor.ConfigDialog(editor, self);
+}
