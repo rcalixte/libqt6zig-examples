096fe6a769c828435eaa08b1afb61fe187b9a97d
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-11-16 18:03:02 -0500

    events: Refactor for improved layout

--- src/events/main.zig
+++ src/events/main.zig
@@ -1,62 +1,75 @@
 const std = @import("std");
 const qt6 = @import("libqt6zig");
+const C = qt6.C;
 const qapplication = qt6.qapplication;
-const qkeyevent = qt6.qkeyevent;
-const qlabel = qt6.qlabel;
-const qmouseevent = qt6.qmouseevent;
-const qnamespace_enums = qt6.qnamespace_enums;
 const qwidget = qt6.qwidget;
-const qwheelevent = qt6.qwheelevent;
+const qlabel = qt6.qlabel;
 const qvboxlayout = qt6.qvboxlayout;
+const qnamespace_enums = qt6.qnamespace_enums;
+const qmouseevent = qt6.qmouseevent;
+const qkeyevent = qt6.qkeyevent;
+
+var label: C.QLabel = undefined;
+var buffer: [64]u8 = undefined;
 
 pub fn main() void {
     const argc = std.os.argv.len;
     const argv = std.os.argv.ptr;
-    _ = qapplication.New(argc, argv);
+    const qapp = qapplication.New(argc, argv);
+    defer qapplication.QDelete(qapp);
 
     const widget = qwidget.New2();
     defer qwidget.QDelete(widget);
 
     qwidget.SetFixedWidth(widget, 400);
     qwidget.SetFixedHeight(widget, 100);
+    qwidget.OnMousePressEvent(widget, mousePressEvent);
+    qwidget.OnKeyPressEvent(widget, keyPressEvent);
 
-    const label = qlabel.New5("Press any key or click the mouse here!", widget);
+    label = qlabel.New3("### Press any key or click the mouse here!");
 
     qlabel.SetFocusPolicy(label, qnamespace_enums.FocusPolicy.StrongFocus);
-    qlabel.OnKeyPressEvent(label, keyPressEvent);
+    qlabel.SetTextFormat(label, qnamespace_enums.TextFormat.MarkdownText);
+    qlabel.SetAlignment(label, qnamespace_enums.AlignmentFlag.AlignCenter);
     qlabel.OnMousePressEvent(label, mousePressEvent);
+    qlabel.OnKeyPressEvent(label, keyPressEvent);
+
+    const layout = qvboxlayout.New2();
+
+    qvboxlayout.AddStretch(layout);
+    qvboxlayout.AddWidget(layout, label);
+    qvboxlayout.AddStretch(layout);
+    qwidget.SetLayout(widget, layout);
 
     qwidget.Show(widget);
 
     _ = qapplication.Exec();
 }
 
-fn mousePressEvent(self: ?*anyopaque, event: ?*anyopaque) callconv(.c) void {
+fn mousePressEvent(_: ?*anyopaque, event: ?*anyopaque) callconv(.c) void {
     const mouse = qmouseevent.Button(event);
-    var buffer: [64]u8 = undefined;
     switch (mouse) {
         qnamespace_enums.MouseButton.LeftButton => {
-            const text = "Left mouse button pressed!";
+            const text = "## Left mouse button pressed!";
             const formatted = std.fmt.bufPrintZ(&buffer, text, .{}) catch @panic("Buffer full");
-            qlabel.SetText(self, formatted);
+            qlabel.SetText(label, formatted);
         },
         qnamespace_enums.MouseButton.RightButton => {
-            const text = "Right mouse button pressed!";
+            const text = "## Right mouse button pressed!";
             const formatted = std.fmt.bufPrintZ(&buffer, text, .{}) catch @panic("Buffer full");
-            qlabel.SetText(self, formatted);
+            qlabel.SetText(label, formatted);
         },
         else => {
-            const text = "Mouse button keycode: {d}";
+            const text = "## Mouse button keycode: {d}";
             const formatted = std.fmt.bufPrintZ(&buffer, text, .{mouse}) catch @panic("Buffer full");
-            qlabel.SetText(self, formatted);
+            qlabel.SetText(label, formatted);
         },
     }
 }
 
-fn keyPressEvent(self: ?*anyopaque, event: ?*anyopaque) callconv(.c) void {
+fn keyPressEvent(_: ?*anyopaque, event: ?*anyopaque) callconv(.c) void {
     const key = qkeyevent.Key(event);
-    var buffer: [64]u8 = undefined;
-    const text = "You pressed key code: {d}";
+    const text = "## You pressed key code: {d}";
     const formatted = std.fmt.bufPrintZ(&buffer, text, .{key}) catch @panic("Buffer full");
-    qlabel.SetText(self, formatted);
+    qlabel.SetText(label, formatted);
 }
