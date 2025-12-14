7c32eb607cc79ef2c7f1dd62c6a9181d34159398
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-12-14 16:32:54 -0500

    Add karchive

---
+++ src/libraries/extras/karchive/main.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const qt6 = @import("libqt6zig");
+const qcoreapplication = qt6.qcoreapplication;
+const kzip = qt6.kzip;
+const qiodevicebase_enums = qt6.qiodevicebase_enums;
+
+const ZIP_FILE = "zig-out/hello.zip";
+var buffer: [64]u8 = undefined;
+
+pub fn main() !void {
+    const argc = std.os.argv.len;
+    const argv = std.os.argv.ptr;
+    const qapp = qcoreapplication.New(argc, argv);
+    defer qcoreapplication.QDelete(qapp);
+
+    const archive = kzip.New(ZIP_FILE);
+    defer kzip.QDelete(archive);
+
+    var stdout_writer = std.fs.File.stdout().writer(&buffer);
+
+    if (kzip.Open(archive, qiodevicebase_enums.OpenModeFlag.WriteOnly)) {
+        defer _ = kzip.Close(archive);
+
+        _ = kzip.WriteFile(archive, "world", "The whole world inside a hello");
+        try stdout_writer.interface.print("Successfully wrote to '{s}'\n", .{ZIP_FILE});
+        try stdout_writer.interface.flush();
+    } else {
+        try stdout_writer.interface.print("Failed to open '{s}' for writing\n", .{ZIP_FILE});
+        try stdout_writer.interface.flush();
+    }
+}
