f2c46cfe160adead31f9a385faadf28c97adca6d
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-12-21 16:54:07 -0500

    multimedia: Add error handling and improve cross-platform support

--- src/libraries/multimedia/main.zig
+++ src/libraries/multimedia/main.zig
@@ -5,18 +5,25 @@ const qmediaplayer = qt6.qmediaplayer;
 const qmediaplayer_enums = qt6.qmediaplayer_enums;
 const qaudiooutput = qt6.qaudiooutput;
 const qurl = qt6.qurl;
-const qcoreapplication = qt6.qcoreapplication;
 
 var buffer: [32]u8 = undefined;
-var stdout_writer = std.fs.File.stdout().writer(&buffer);
 
 pub fn main() !void {
     const argc = std.os.argv.len;
     const argv = std.os.argv.ptr;
-    _ = qapplication.New(argc, argv);
+    const qapp = qapplication.New(argc, argv);
+    defer qapplication.QDelete(qapp);
+
+    var stdout_writer = std.fs.File.stdout().writer(&buffer);
 
     const player = qmediaplayer.New();
     defer qmediaplayer.QDelete(player);
+
+    if (qmediaplayer.Error(player) != qmediaplayer_enums.Error.NoError) {
+        try stdout_writer.interface.writeAll("Failed to create player.\n");
+        try stdout_writer.interface.flush();
+        return;
+    }
 
     const output = qaudiooutput.New();
     defer qaudiooutput.QDelete(output);
@@ -37,12 +44,14 @@ pub fn main() !void {
 }
 
 fn onPlaybackStateChanged(_: ?*anyopaque, state: i32) callconv(.c) void {
+    var stdout_writer = std.fs.File.stdout().writer(&buffer);
+
     stdout_writer.interface.print("Playback state: {any}\n", .{state}) catch @panic("Playback state stdout error");
     stdout_writer.interface.flush() catch @panic("Failed to flush stdout writer");
 
     if (state == qmediaplayer_enums.PlaybackState.StoppedState) {
         stdout_writer.interface.writeAll("Playback complete.\n") catch @panic("Playback complete stdout error");
         stdout_writer.interface.flush() catch @panic("Failed to flush stdout writer");
-        qcoreapplication.Exit();
+        qapplication.Exit();
     }
 }
